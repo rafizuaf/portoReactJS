07978b2dd84867b782654b437506fee26d017539
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2023-01-26 19:20:01 +0700

    project done

---
+++ myporto/src/data/data.js
@@ -0,0 +1,37 @@
+// import WorkIm from '../assets/projects/workImg.jpeg'
+// import RealEst from '../assets/projects/realestate.jpg'
+import Kalibrr from '../assets/projects/Kalibrr.JPG'
+import WebPorto from '../assets/projects/WebPorto.JPG'
+
+export const data=[
+    {
+        id:1,
+        name:"Job Portal Clone",
+        image:Kalibrr,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Portofolio Web Page",
+        image:WebPorto,
+        github:"",
+        live:"",
+    },
+    // {
+    //     id:3,
+    //     name:"React JS Application",
+    //     image:WorkIm,
+    //     github:"",
+    //     live:"",
+    // },
+    // {
+    //     id:4,
+    //     name:"React JS Application",
+    //     image:RealEst,
+    //     github:"",
+    //     live:"",
+    // },
+
+
+]
